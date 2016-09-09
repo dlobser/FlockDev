02a1711fbb4e0bfb24fb609f91e850a4e8fbe137
ckromero-flintny <chris@adoptive.co>
2016-09-09 17:00:42 -0400

    runs on android with new environment

--- Assets/Dev/DL/Scripts/Utility/MergeMesh.cs
+++ Assets/Dev/DL/Scripts/Utility/MergeMesh.cs
@@ -1,25 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
-public class MergeMesh : MonoBehaviour {
-	public string outputPath = "Assets/mesh.asset";
-	void Start() {
-		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
-		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
-		int i = 0;
-		while (i < meshFilters.Length) {
-			combine[i].mesh = meshFilters[i].sharedMesh;
-			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-			meshFilters[i].gameObject.active = false;
-			i++;
-		}
-		transform.GetComponent<MeshFilter>().mesh = new Mesh();
-		transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
-		AssetDatabase.CreateAsset( transform.GetComponent<MeshFilter>().mesh, outputPath);
-		AssetDatabase.SaveAssets();
-		transform.gameObject.active = true;
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//
+//[RequireComponent(typeof(MeshFilter))]
+//[RequireComponent(typeof(MeshRenderer))]
+//public class MergeMesh : MonoBehaviour {
+//	public string outputPath = "Assets/mesh.asset";
+//	void Start() {
+//		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+//		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+//		int i = 0;
+//		while (i < meshFilters.Length) {
+//			combine[i].mesh = meshFilters[i].sharedMesh;
+//			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+//			meshFilters[i].gameObject.active = false;
+//			i++;
+//		}
+//		transform.GetComponent<MeshFilter>().mesh = new Mesh();
+//		transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+//		AssetDatabase.CreateAsset( transform.GetComponent<MeshFilter>().mesh, outputPath);
+//		AssetDatabase.SaveAssets();
+//		transform.gameObject.active = true;
+//	}
+//}
