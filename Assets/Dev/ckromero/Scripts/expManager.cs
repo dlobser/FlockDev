c1b91ce9124cbaedf3ed19e8e2ad0e816b16e3d4
ckromero <chris@adoptive.co>
2016-09-11 15:01:54 -0400

    initial global control with audio muting enabled.

---
+++ Assets/Dev/ckromero/Scripts/expManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class expManager : MonoBehaviour {
+	//GLOBAL SETTINGS
+	public bool debugOn;
+
+	//ENVIRONMENT SETTINGS
+	public Environment environment;
+	public bool environmentStatus=true;
+
+
+
+	//AUDIO SETTINGS
+	public AudioMixerGroup Master;
+	public bool muteAudio=false;
+	private float storedVolume;
+	private float startVolume;
+	private const float NOVOLUME = -80.0f;
+
+
+
+
+
+
+//	public ZoneManager zonemanager;
+//	public MommaBird mommabird;
+//	public Food food;
+//	public ActionTree actionTree;
+//	public Mirror mirror;
+//	public DeathBed deathbed;
+//	public GestureManager gestureManager;
+//	public MatingSpot matingSpot;
+//	public CliffsEdge cliffsEdge;
+	
+
+
+
+	// Use this for initialization
+	void Start () {
+		Master.audioMixer.GetFloat("MasterVolume",out startVolume);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ENVIRONMENT
+		if (environmentStatus == false) {
+			environment.enabled = false;
+		}
+
+		//AUDIO
+		if (debugOn) Debug.Log (storedVolume.ToString ());
+
+		if (muteAudio == true) {
+
+			Master.audioMixer.SetFloat ("MasterVolume", NOVOLUME);
+		} else {
+			Master.audioMixer.SetFloat("MasterVolume",startVolume);
+		}
+	}
+}
