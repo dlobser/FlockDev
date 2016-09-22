49cc99fc55c061a2d323ebbcc1181032f94cf285
david lobser <davidlobser@yahoo.com>
2016-09-22 14:55:57 -0400

    fader manager and choose headset

---
+++ Assets/Dev/DL/Scripts/FaderMaterialColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaderMaterialColor : Fader {
+
+	public Color[] _Color;
+	public Material mat;
+
+	public override void Fade(){
+		float currentLevel =( Mathf.Clamp( level,min,max) / levels)*_Color.Length;
+		Color thisColor = _Color [(int)Mathf.Clamp(Mathf.Floor (currentLevel),0,_Color.Length-1)];
+		Color nextColor = _Color [(int)Mathf.Clamp(Mathf.Ceil (currentLevel),0,_Color.Length-1)];
+		Color matColor = Color.Lerp (thisColor, nextColor, currentLevel - Mathf.Floor (currentLevel));
+		mat.SetColor ("_Color", matColor);
+	}
+}
