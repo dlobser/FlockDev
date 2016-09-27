2435bc9488e3f1c240e43463265490d483b0ec5e
david lobser <davidlobser@yahoo.com>
2016-09-27 15:13:38 -0400

    many new bugs

---
+++ Assets/Dev/DL/Scripts/Utility/scaleBackUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scaleBackUp : MonoBehaviour {
+
+	float initialScale;
+	SphereCollider sphere;
+	public float speed = .5f;
+	// Use this for initialization
+	void Start () {
+		sphere = GetComponent<SphereCollider> ();
+		initialScale = sphere.radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sphere.radius < initialScale) {
+			sphere.radius += Time.deltaTime * speed;
+		}
+	}
+}
