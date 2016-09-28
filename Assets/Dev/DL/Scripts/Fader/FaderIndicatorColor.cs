08a42089f98f7824055c3ba189cfe29beba5a5ef
david lobser <davidlobser@yahoo.com>
2016-09-28 13:30:18 -0400

    updated scene 1

--- Assets/Dev/DL/Scripts/Fader/FaderIndicatorColor.cs
+++ Assets/Dev/DL/Scripts/Fader/FaderIndicatorColor.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FaderMaterialColor : Fader {
+public class FaderIndicatorColor : Fader {
 
 	public Color[] _Color;
-	public Material mat;
-	public string colorName;
+	Material mat;
+	public GameObject indicator;
+
+	void Start(){
+		mat = indicator.GetComponent<MeshRenderer> ().material;
+	}
 
 	public override void Fade(){
+		
 		float currentLevel =( Mathf.Clamp( level,min,max) / levels)*_Color.Length;
 		Color thisColor = _Color [(int)Mathf.Clamp(Mathf.Floor (currentLevel),0,_Color.Length-1)];
 		Color nextColor = _Color [(int)Mathf.Clamp(Mathf.Ceil (currentLevel),0,_Color.Length-1)];
 		Color matColor = Color.Lerp (thisColor, nextColor, currentLevel - Mathf.Floor (currentLevel));
-		mat.SetColor ("colorName", matColor);
+
+		if (matColor.a > 0) {
+			if(!indicator.activeInHierarchy)
+				indicator.SetActive(true);
+			mat.SetColor ("_Color", matColor);
+		} else if (indicator.activeInHierarchy) {
+			indicator.SetActive (false);
+		}
+
 	}
 }
---
+++ Assets/Dev/DL/Scripts/Fader/FaderMaterialSwapper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaderMaterialSwapper : Fader {
+
+	public Material[] materials;
+	public GameObject obj;
+	int which = 0;
+	MeshRenderer rend;
+	Vector2 range = Vector2.zero;
+
+	void Start(){
+		rend = obj.GetComponent<MeshRenderer> ();
+		setRange (level);
+	}
+
+	void setRange(float val){
+		range = new Vector2 (Mathf.Floor (val) - .5f, Mathf.Floor (val) + 1);
+	}
+
+
+	public override void Fade(){
+		if (level > range.y || level < range.x) {
+			setRange (level);
+			float currentLevel = (Mathf.Clamp (level, min, max) / levels) * materials.Length;
+			Material thisMat = materials [(int)Mathf.Clamp (Mathf.Floor (currentLevel), 0, materials.Length - 1)];
+			rend.material = thisMat;
+		}
+	}
+}
