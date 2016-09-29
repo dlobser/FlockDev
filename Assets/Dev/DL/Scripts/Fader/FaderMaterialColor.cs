5611b3dc854b20c371f6d4425922f3ba819347c2
david lobser <davidlobser@yahoo.com>
2016-09-29 11:38:23 -0400

    updated 1B

--- Assets/Dev/DL/Scripts/Fader/FaderMaterialColor.cs
+++ Assets/Dev/DL/Scripts/Fader/FaderMaterialColor.cs
@@ -6,12 +6,17 @@ public class FaderMaterialColor : Fader {
 	public Color[] _Color;
 	public Material mat;
 	public string colorName;
+	public bool interpolate = true;
 
 	public override void Fade(){
 		float currentLevel =( Mathf.Clamp( level,min,max) / levels)*_Color.Length;
 		Color thisColor = _Color [(int)Mathf.Clamp(Mathf.Floor (currentLevel),0,_Color.Length-1)];
-		Color nextColor = _Color [(int)Mathf.Clamp(Mathf.Ceil (currentLevel),0,_Color.Length-1)];
-		Color matColor = Color.Lerp (thisColor, nextColor, currentLevel - Mathf.Floor (currentLevel));
-		mat.SetColor (colorName, matColor);
+		if (interpolate) {
+			Color nextColor = _Color [(int)Mathf.Clamp (Mathf.Ceil (currentLevel), 0, _Color.Length - 1)];
+			Color matColor = Color.Lerp (thisColor, nextColor, currentLevel - Mathf.Floor (currentLevel));
+			mat.SetColor (colorName, matColor);
+		} else {
+			mat.SetColor (colorName, thisColor);
+		}
 	}
 }
