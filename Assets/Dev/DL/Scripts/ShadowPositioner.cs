5bef42dfafec803ff6ccbbb55245f0f9558f5717
david lobser <davidlobser@yahoo.com>
2016-09-30 18:04:43 -0400

    master scene update

---
+++ Assets/Dev/DL/Scripts/ShadowPositioner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowPositioner : MonoBehaviour {
+
+	public float height;
+	public Transform target;
+	Vector3 pos;
+	SettingsManager settings;
+	Color col;
+	Vector3 scale;
+
+	void Start(){
+		settings = GameObject.Find ("SettingsManager").GetComponent<SettingsManager> ();
+	}
+	void Update(){
+		SetPosition ();
+
+	}
+	// Update is called once per frame
+	void SetPosition () {
+		pos.Set (target.position.x, height, target.position.z);
+		this.transform.position = pos;
+		float size = settings.settingsJSON.shadowSize;
+		scale.Set (size, size, size);
+		this.transform.localScale = scale;
+		Vector4 colVec = settings.settingsJSON.shadowColor;
+		col = new Color (colVec.x, colVec.y, colVec.z, colVec.w);
+		this.GetComponent<SpriteRenderer> ().color = col;
+	}
+}
