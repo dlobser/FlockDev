915ab4b3546771da7da778cf3e73a94c81acbacf
ckromero <chris@adoptive.co>
2016-09-30 19:07:32 -0400

    really

--- Assets/Dev/ckromero/Scripts/SettingsManager.cs
+++ Assets/Dev/ckromero/Scripts/SettingsManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using Holojam.Tools;
 
-public class SettingsManager : MonoBehaviour {
+public class SettingsManager :  Synchronizable {
 
 	public SettingsJSON settingsJSON;
 	private Synchronizable synchronizable;
@@ -14,9 +14,11 @@ public class SettingsManager : MonoBehaviour {
 	}
 
 	private void UpdateSettings() { 
-
-		synchronizable.synchronizedString = JsonUtility.ToJson (settingsJSON);
-
+		if (sending) {
+			synchronizable.synchronizedString = JsonUtility.ToJson (settingsJSON);
+		} else {
+			settingsJSON = JsonUtility.FromJson<SettingsJSON> (synchronizable.synchronizedString);
+		}
 	}
 
 	void Update() { 
