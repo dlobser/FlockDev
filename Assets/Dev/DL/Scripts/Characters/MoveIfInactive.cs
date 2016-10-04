3dd38932782b515e363e45807fb3bb144a492833
david lobser <davidlobser@yahoo.com>
2016-10-04 10:50:43 -0400

    scene updates

--- Assets/Dev/DL/Scripts/Characters/MoveIfInactive.cs
+++ Assets/Dev/DL/Scripts/Characters/MoveIfInactive.cs
@@ -6,7 +6,7 @@ public class MoveIfInactive : MonoBehaviour {
 	Vector3 maximum = new Vector3(-10000,10000,0);
 	float[] counters;
 	public float missingTrackingTime = 2;
-	// Use this for initialization
+
 	void Start () {
 		
 		counters = new float[this.transform.childCount];
@@ -17,7 +17,6 @@ public class MoveIfInactive : MonoBehaviour {
 		while (true) {
 			for (int i = 0; i < this.transform.childCount; i++) {
 				bool tracked = this.transform.GetChild (i).gameObject.GetComponent<Holojam.Tools.Actor> ().view.IsTracked;
-//				Debug.Log ("tracked" + tracked);
 				if (!tracked) {
 					counters [i] += Time.deltaTime;
 					if (counters [i] > missingTrackingTime)
@@ -25,8 +24,9 @@ public class MoveIfInactive : MonoBehaviour {
 				} else if (tracked) {
 					counters [i] = 0;
 				}
-				yield return new WaitForSeconds (0);
+
 			}
+			yield return null;
 		}
 	}
 }
