40b00820970960aed29d5c6b88a4ac5b3964af61
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-03-12 13:36:14 -0400

    Updated RM binds

--- Assets/Wenbo/ResetManager.cs
+++ Assets/Wenbo/ResetManager.cs
@@ -6,44 +6,46 @@ using System;
 using Holojam.Tools;
 
 public class ResetManager : MonoBehaviour {
-	
-	public SynchronizedString synchronizable;
-	public TapToReset tap;
-	string[] signal;
-	// Use this for initialization
-	void Start () {
-		signal = new string[7] { "1", "2", "3", "4","5","6","ALL" };
-	}
+  
+  public SynchronizedString synchronizable;
+  public TapToReset tap;
+  string[] signal;
+  // Use this for initialization
+  void Start () {
+    signal = new string[7] { "1", "2", "3", "4","5","6","ALL" };
+  }
 
-	// Update is called once per frame
-	void Update () {
-		string text = "";
-		if (Input.GetKeyDown(KeyCode.Alpha0))
-			text = signal [0];
-		if (Input.GetKeyDown(KeyCode.Alpha1))
-			text = signal [1];
-		if (Input.GetKeyDown(KeyCode.Alpha2))
-			text = signal [2];
-		if (Input.GetKeyDown(KeyCode.Alpha3))
-			text = signal [3];
+  // Update is called once per frame
+  void Update () {
+    string text = "";
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+      text = signal [0];
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+      text = signal [1];
+    if (Input.GetKeyDown(KeyCode.Alpha3))
+      text = signal [2];
     if (Input.GetKeyDown(KeyCode.Alpha4))
-      text = signal[4];
+      text = signal [3];
     if (Input.GetKeyDown(KeyCode.Alpha5))
-      text = signal[5];
+      text = signal[4];
     if (Input.GetKeyDown(KeyCode.Alpha6))
-			text = signal [6];
-		if (BuildManager.IsMasterClient()) {
-			synchronizable.SetText(text);
-		} else {
-			text = synchronizable.GetText();
-			int index;
-			Int32.TryParse (text,out index);
-			if (index == BuildManager.BUILD_INDEX || text == "ALL") {
-				tap.ButtonReset ();
-			}
-		}
-		Debug.Log (text);
+      text = signal[5];
 
-	}
+    if (Input.GetKeyDown(KeyCode.A))
+      text = signal [6];
+
+    if (BuildManager.IsMasterClient()) {
+      synchronizable.SetText(text);
+    } else {
+      text = synchronizable.GetText();
+      int index;
+      Int32.TryParse (text,out index);
+      if (index == BuildManager.BUILD_INDEX || text == "ALL") {
+        tap.ButtonReset ();
+      }
+    }
+    Debug.Log (text);
+
+  }
 
 }
--- Assets/etc/SynchronizedString.cs
+++ Assets/etc/SynchronizedString.cs
@@ -18,10 +18,4 @@ public class SynchronizedString : Holojam.Tools.Synchronizable{
 
    public string GetText() { return data.text; }
    public void SetText(string t) { data.text = t; }
-
-   //public Vector3 GetTriple() { return data.vector3s[0]; }
-   //public void SetTriple(Vector3 t) { data.vector3s[0] = t; }
-
-   //public Quaternion GetQuad() { return data.vector4s[0]; }
-   //public void SetQuad(Quaternion q) { data.vector4s[0] = q; }
 }
